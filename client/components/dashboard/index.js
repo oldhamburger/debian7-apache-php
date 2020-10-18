263bfbc2f7f01c605c4dc75f2e78ecee1488786f
oldhamburger <112925121+oldhamburger@users.noreply.github.com>
2020-10-18 15:06:24 +0000

    repository

--- client/components/dashboard/index.js
+++ client/components/dashboard/index.js
@@ -1,3 +1,38 @@
 import React, { Component } from "react";
 import Summary from "./Summary";
-import C
+import ConnectedPlaidAccount from "../PlaidAccount";
+import PlaidAccountTransactions from "../dashboard/Transaction";
+import InsightCard from "./insights/InsightCards";
+import { connect } from "react-redux";
+import CategoriesDonut from "./CategoriesDonutChart";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+import {
+  gettingAccounts,
+  gettingTransactions,
+  gettingBalance
+} from "../../store/accountReducer";
+import {
+  getLargest,
+  getRestaurantSpend,
+  getMerchantSpend,
+  getTranspoSpend,
+  getFees
+} from "../../store/insightReducer";
+
+import Loading from "./Loading";
+
+import { logoutUser } from "../../store/userReducer";
+import "../dashboard/Summary.css";
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false
+    };
+  }
+  async componentDidMount() {
+    await this.props.gettingAccounts();
+    const { accounts } = this.props;
+    if (this.props.accounts.length) {
+ 
