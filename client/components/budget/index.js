d5cf6675d6d59a4ec8f76aaba1926be8dd32a27b
oldhamburger <112925121+oldhamburger@users.noreply.github.com>
2020-10-18 07:17:04 +0000

    lecture9: updates

--- client/components/budget/index.js
+++ client/components/budget/index.js
@@ -31,4 +31,39 @@ class BudgetIndex extends Component {
           <div className="budgBack">
             <div className="spacer" />
             <BudgetSummary />
-            <div className="visContainer
+            <div className="visContainer">
+              <BudgetVis />
+            </div>
+            <Footer />
+          </div>
+        ) : (
+          <div className="loading">
+            <Loading />
+            <h2 className="loading">Calculating your budget</h2>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+const mapState = state => ({
+  budget: state.budgetReducer.budget,
+  income: state.accountReducer.monthlyIncome,
+  user: state.userReducer.user,
+  accounts: state.accountReducer.accounts,
+  transactions: state.accountReducer.transactions,
+  budget: state.budgetReducer.budget,
+  spendingLimit: state.budgetReducer.spendingLimit,
+});
+const mapDispatchToProps = dispatch => ({
+  gettingIncome: accounts => dispatch(gettingIncome(accounts)),
+  gettingAccounts: () => dispatch(gettingAccounts()),
+  gettingTransactions: accounts => dispatch(gettingTransactions(accounts)),
+  gettingBudget: (userId, monthlyIncome) =>
+    dispatch(gettingBudget(userId, monthlyIncome)),
+});
+export default connect(
+  mapState,
+  mapDispatchToProps
+)(BudgetIndex);
