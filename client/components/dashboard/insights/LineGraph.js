d0ed7559a4e8e3fa44281e904dfbfdcdff332f88
oldhamburger <112925121+oldhamburger@users.noreply.github.com>
2020-10-26 12:42:09 +0000

    Begin development on next version [skip ci]

--- client/components/dashboard/insights/LineGraph.js
+++ client/components/dashboard/insights/LineGraph.js
@@ -7,4 +7,60 @@ import { connect } from 'react-redux';
 
 const data = {
   labels: [],
-  dataset
+  datasets: [
+    {
+      data: [0, 0, 0],
+      label: 'Food and Drink',
+      borderColor: '#A8DADC',
+      hoverBorderColor: '#40bcc1',
+      fill: false,
+    },
+    {
+      data: [0, 0, 0],
+      label: 'Shops',
+      borderColor: '#f9bd49',
+      hoverBorderColor: '#dda412',
+      fill: false,
+    },
+    {
+      data: [0, 0, 0],
+      label: 'Travel',
+      borderColor: '#1D3557',
+      hoverBorderColor: '#04142b',
+      fill: false,
+    },
+    {
+      data: [0, 0, 0],
+      label: 'Recreation',
+      borderColor: '#E63946',
+      hoverBorderColor: '#8e3339',
+      fill: false,
+    },
+  ],
+};
+
+const options = {
+  legend: {
+    display: true,
+    position: 'bottom',
+    labels: {
+      fontColor: 'black',
+    },
+  },
+  layout: {
+    padding: {
+      left: 30,
+      right: 50,
+      top: 50,
+      bottom: 40,
+    },
+    maintainAspectRatio: false,
+    responsive: true,
+  },
+};
+
+class LineGraph extends React.Component {
+  constructor() {
+    super();
+    this.state = { data: data };
+    this.populateData
