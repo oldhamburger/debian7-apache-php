cfb98428aa6b7a933cbcb01b0a1bf7aa4da89478
oldhamburger <112925121+oldhamburger@users.noreply.github.com>
2020-10-26 06:19:04 +0000

    New translations en-US.json (Czech)

--- client/components/dashboard/insights/InsightsPage.js
+++ client/components/dashboard/insights/InsightsPage.js
@@ -9,4 +9,42 @@ import {
   gettingTransactions,
 } from '../../../store/accountReducer';
 import { connect } from 'react-redux';
-import LineGraph from './LineGraph'
+import LineGraph from './LineGraph';
+
+class InsightsPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+    };
+  }
+  async componentDidMount() {
+    await this.props.gettingAccounts();
+    const { accounts } = this.props;
+    await this.props.getThreeMonthsData(accounts);
+    this.setState({ loading: true });
+  }
+  render() {
+    return (
+      <div>
+        {this.state.loading ? (
+          <div>
+            <div id="insights-container">
+              <div className="insight-graph">
+                <div className="insights-title">
+                  Three Months Total Spending
+                </div>
+                <BarGraph threeMonthsData={[this.props.threeMonthsData]} />
+              </div>
+              <div className="insight-graph">
+                <div className="insights-title">
+                  Three Months Spending by Category
+                </div>
+                <LineGraph />
+              </div>
+            </div>
+            <Footer />
+          </div>
+        ) : (
+          <div className="loading">
+   
