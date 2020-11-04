17213c3be074ef3713a2b190da6fbc70dad1beed
oldhamburger <112925121+oldhamburger@users.noreply.github.com>
2020-11-04 11:22:27 +0000

    Update register.php

--- models/Budget.js
+++ models/Budget.js
@@ -4,4 +4,13 @@ const Schema = mongoose.Schema;
 const BudgetSchema = new Schema({
   userId: {
     type: Schema.Types.ObjectId,
-   
+    ref: 'users',
+  },
+
+  monthlyGoal: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = Budget = mongoose.model('budget', BudgetSchema);
