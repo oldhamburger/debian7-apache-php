39f7c9c4afc94a1a555a058fcf0443ddb3e93704
oldhamburger <112925121+oldhamburger@users.noreply.github.com>
2023-01-09 09:48:30 +0000

    contributors

--- routes/api/user.js
+++ routes/api/user.js
@@ -25,4 +25,41 @@ router.post("/register", async (req, res, next) => {
       return res.status(400).send({ email: "Email already exists" });
     }
     if (req.body.name === "" || req.body.password === "") {
-      return r
+      return res.status(400).send({ name: "Cannot save empty string as name" });
+    }
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    });
+
+    //Hash password before saving in database
+    const salt = await bcrypt.genSalt(10);
+    const hashed = await bcrypt.hash(newUser.password, salt);
+    newUser.password = hashed;
+    const savedUser = await newUser.save();
+    res.json(savedUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+  try {
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //Find user by email
+    const foundUser = await User.findOne({ email });
+    if (!foundUser) {
+      return res.status(404).json({ emailnotfound: "Email not found" });
+    }
+
+    //Check password
+    const isMatch = await bcrypt.compare(password, foundUser.password);
+    if (
